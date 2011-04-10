480e3e9ac30c32c93664a9968337f004360f284f
Robert <rwalter83@gmail.com>
2011-04-10 19:16:36 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -I/usr/include/ni -lOpenNI -lboost_thread -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_calib3d -o KinectCalib *.cpp
+g++ -I/usr/include/ni -lOpenNI -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_calib3d -o KinectCalib *.cpp
