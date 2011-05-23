48a0e05e9adf2fe3c06dc91cc8861d546b1482ac
Robert Walter <rwalter83@gmail.com>
2011-05-24 00:06:28 +0200

    opencv 2.1 support, added check mode, colored point cloud JSON export

--- KinectCalib.cpp
+++ KinectCalib.cpp
@@ -4,56 +4,10 @@
 // Version     : 0.something
 // Description : interactive calibration tool for kinect
 //============================================================================
-/* estimates camera matrix, lens distortion coefficients of IR and RGB camera
- * as well as extrinsic stereo calibration including rotation and translation 
- * between sensor and essential- plus fundamental matrix.
- *
- * requires openCV 2.2!
- *
- * uses a planar b/w chessboard pattern as calibration object. you can simply
- * print one your and and glue it onto a paperboard to improve the results.
- * just be sure to adjust the following constants in the code:
- * patternSize, patternTileWidth, patternTileWidth
- *
- * depth image acquistion is not used for the calibration! only raw IR and
- * RGB images are used. as the structured light pattern of the IR projector
- * may trouble the pattern detection it is highly recommended to simply
- * cover the projector (leftmost lens) of the kinect. in this case you have
- * to provide other IR light sources. sunlight is just perfect but also some
- * lamps (usually not energy saving lamps) emmit a decent amount of IR light. 
- * you have to experiment a little bit. 
- *
- * for pattern detection and tracking, the 16 bit IR image needs to be 
- * quantized down to 8 bits. use the alpha slider to optimize the brightness
- * of the target image!
- *
- * key mapping:
- *
- *		'i'				switch to IR mode (calibrate IR camera)
- *
- * 		'r'				switch to RGB mode (calibrate RGB camera)
- *
- *		'q'				switch to stereo mode (stereo calibration)
- *							requires IR and RGB camera to be calibrated well!
- *		
- *		't'				toggles calibration pattern tracking mode
- *
- *		SPACE			captures a view and calibrates based on all already
- *							captured views
- *		
- *		's'				saves calibration data (calib.xml)
- *
- *		'l'				loads calibration data (calib.xml)
- *
- * TODOs:
- *		
- *		- figure out why results are still very poor (at least for me)
- *		- better feedback of stereo calibration quality (depth+IR overlay)
- *
- */
 
 #include <iostream>
 #include <sstream>
+#include <fstream>
 using namespace std;
 
 #include <opencv/cv.h>
@@ -79,7 +33,8 @@ using namespace xn;
 enum CalibrationMode {
 	CalibrationIr,
 	CalibrationRgb,
-	CalibrationStereo
+	CalibrationStereo,
+	CalibrationCheck
 };
 
 /*//////////////////////////////////////////////////////////////////////
@@ -91,12 +46,16 @@ const float patternTileWidth = 0.0289f; // in meters
 const float patternTileHeight = 0.0289f;
 
 const Size frameSize(640, 480); // TODO make use of it!
+const Size subPixWindowSize(11, 11); 
+const Size subPixZeroZone(-1, -1);
+const TermCriteria subPixTerm(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1);
 
 const char *frameNamePrimary = "Primary Frame";
 const char *frameNameSecondary = "Secondary Frame";
 
 const Scalar textColorRed(0,0,255);
 const Scalar textColorBlue(255,0,0);
+const Scalar textColorWhite(255,255,255);
 const int textFont = FONT_HERSHEY_SIMPLEX;
 const float textScale = 1.0f;
 const int textIndent = 10;
@@ -134,7 +93,7 @@ Mat rgbCameraMatrix;
 Mat rgbDistCoeffs;
 
 // stereo calibration
-Mat R,T,E,F;
+Mat R,T,E,F,rvec;
 
 // image buffers
 Mat irMat16(480, 640, CV_16UC1);
@@ -143,10 +102,14 @@ Mat irMat8Rgb(480, 640, CV_8UC3);
 Mat irMat8RgbUndist(480, 640, CV_8UC1);
 
 Mat rgbMat(480, 640, CV_8UC3); // note: this is actually BGR
+Mat rgbMatGray(480, 640, CV_8UC1);
 Mat rgbMatUndist(480, 640, CV_8UC3);
 
 Mat depthMat16(480, 640, CV_16UC1);
-Mat depthMat8(480, 640, CV_8UC1);	
+Mat depthMat8(480, 640, CV_8UC1);
+
+Mat overlayMat(480, 640, CV_8UC3);
+Mat overlayCalibMat(480, 640, CV_8UC3);
 
 /*//////////////////////////////////////////////////////////////////////
 // functions
@@ -159,6 +122,12 @@ void acquireIr(float alpha) {
 	cvtColor(irMat8, irMat8Rgb, CV_GRAY2RGB);
 }
 
+void acquireDepth(float alpha) {
+	Mat mat(frameSize, CV_16UC1, (unsigned char*) depth.GetDepthMap());
+	mat.copyTo(depthMat16);
+	depthMat16.convertTo(depthMat8, CV_8UC1, alpha);
+}
+
 void textLine(Mat &img, const string line, int lineNumber, Scalar color) {
 	putText(img, line, Point(textIndent,lineNumber*textHeight), textFont, textScale, color);
 }
@@ -166,6 +135,49 @@ void textLine(Mat &img, const string line, int lineNumber, Scalar color) {
 void acquireRgb() {
 	Mat rgbTemp(frameSize, CV_8UC3, (unsigned char*) image.GetImageMap());
 	cvtColor(rgbTemp, rgbMat, CV_RGB2BGR);
+	cvtColor(rgbTemp, rgbMatGray, CV_RGB2GRAY);
+}
+
+void dump(Mat &m) {
+	assert(m.type() == CV_64F);
+	for (int j=0; j<m.size().height; j++) {
+		for (int k=0; k<m.size().width; k++) {
+			cout << (double) m.at<double>(j,k) << " ";
+		}
+		cout << endl;
+	}
+	cout <<  endl;
+}
+
+void dump(vector<Mat> &v) {
+	for (int i=0; i<v.size(); i++) {
+		assert(v[i].type()  == CV_64F);
+		for (int j=0; j<v[i].size().height; j++) {
+			for (int k=0; k<v[i].size().width; k++) {
+				cout << (double) v[i].at<double>(j,k) << " ";
+			}
+			cout << endl;
+		}
+		cout <<  endl;
+	}
+}
+
+void overlay(Mat &rgb, Mat &gray, Mat &overlay) {
+	unsigned char *rgbData = (unsigned char *) rgb.data;
+	unsigned char *grayData = (unsigned char *) gray.data;
+	unsigned char *overlayData = (unsigned char *) overlay.data;
+
+	int n = frameSize.width * frameSize.height;
+	for (int i=0; i<n; i++) {
+		if (grayData[i] == 0) {
+			//overlayData[3*i+0] = grayData[i] == 0 ? 255 : 0;
+			overlayData[3*i+1] = (rgbData[3*i+0] + rgbData[3*i+1] + rgbData[3*i+2]) / 3;
+			overlayData[3*i+2] = 0;
+		} else {
+			overlayData[3*i+1] = (rgbData[3*i+0] + rgbData[3*i+1] + rgbData[3*i+2]) / 3;
+			overlayData[3*i+2] = 255 - grayData[i];
+		}
+	}
 }
 
 void initCaptureMode(bool initImage, bool initDepth, bool initIR) {
@@ -210,8 +222,77 @@ void initCaptureMode(bool initImage, bool initDepth, bool initIR) {
 	cout << "context.StartGeneratingAll : " << xnGetStatusString(nRetVal) << endl;
 }
 
-void projectDepthToRgb(const Mat &src, Mat &dst, Mat R, Mat T, Mat irCameraMatrix, Mat irDistCoeffs, Mat rgbCameraMatrix, Mat rgbDistCoeffs) {
+void reconstruct(Mat &depthMat, Mat &cameraMatrix, Mat &distCoeffsMat, vector<Point3f> &points) {
+	int width = depthMat.size().width;
+	int height = depthMat.size().height;
+
+	unsigned short *depth = (unsigned short *) depthMat.data;
+	double *dist = (double *) distCoeffsMat.data;
+
+	// undistort points (only once)
+	static vector<Point2f> imgCoordsDist(width*height);
+	static vector<Point2f> imgCoordsUndist(width*height);
+	static bool firstCall = true;
+
+	if (firstCall) {
+		for (int u=0; u<width; u++) {
+			for (int v=0; v<height; v++) {
+				int i = u + v*width;
+				imgCoordsDist[i] = Point2f(u,v);
+			}
+		}
+		undistortPoints(Mat(imgCoordsDist), imgCoordsUndist, cameraMatrix, distCoeffsMat);
+		firstCall = false;
+	}
+	//dump(imgCoordsUndist, 25);
+
+	// reconstruct 3d coordinates
+	for (int u=0; u<width; u++) {
+		for (int v=0; v<height; v++) {
+			int i = u + v*width;
+			float xh = imgCoordsUndist[i].x;
+			float yh = imgCoordsUndist[i].y;
+
+			float Z = depth[i] / 1000.0f; // TODO pythagoras
+
+			points[i] = Point3f(xh*Z, yh*Z, Z);
+		}
+	}
+}
+
+void mapDepthToRgb(Mat &src, Mat &dst, Mat rvec, Mat T, Mat irCameraMatrix, Mat irDistCoeffs, Mat rgbCameraMatrix, Mat rgbDistCoeffs) {
 	// TODO
+	static int n = frameSize.width*frameSize.height;
+	static int width = frameSize.width;
+	static vector<Point3f> reconstructedPoints(n);
+	static vector<Point2f> projectedPoints(n);
+
+	// reconstruct 3d points
+	reconstruct(src, irCameraMatrix, irDistCoeffs, reconstructedPoints);
+
+	// project 3d points to rgb coordinates
+	projectPoints(Mat(reconstructedPoints), -rvec, -T, rgbCameraMatrix, rgbDistCoeffs, projectedPoints);
+
+	// create calibrated depth map
+	unsigned short *srcData = (unsigned short *) src.data;
+	unsigned short *dstData = (unsigned short *) dst.data;
+	for (int i=0; i<n; i++) {
+		int u = (int) projectedPoints[i].x;
+		int v = (int) projectedPoints[i].y;
+		int j = u + v*width;
+		if (j >= 0 && j < 640*480 && srcData[i] > 0) {
+			//if (srcData[i] > dstData[j]) {
+			if (dstData[j] == 0) {
+				dstData[j] = srcData[i];
+			} else {
+				if (dstData[j] > srcData[i]) {
+					dstData[j] = srcData[i];
+				}
+			}
+				
+			//}
+		}
+	}
 }
 
 void genPatternObjectPoints(Size patternSize, float dx, float dy, vector<Point3f> &objectPoints) {
@@ -225,14 +306,14 @@ void genPatternObjectPoints(Size patternSize, float dx, float dy, vector<Point3f
 
 void saveCalibration(const char *fname) {
 	FileStorage fs(fname, FileStorage::WRITE);
-	fs << "irCameraMatrix" << irCameraMatrix;
-	fs << "irDistCoeffs" << irDistCoeffs;
-	fs << "rgbCameraMatrix" << rgbCameraMatrix;
-	fs << "rgbDistCoeffs" << rgbDistCoeffs;
-	fs << "R" << R;
-	fs << "T" << T;
-	fs << "E" << E;
-	fs << "F" << F;
+	if (!irCameraMatrix.empty()) fs << "irCameraMatrix" << irCameraMatrix;
+	if (!irDistCoeffs.empty()) fs << "irDistCoeffs" << irDistCoeffs;
+	if (!rgbCameraMatrix.empty()) fs << "rgbCameraMatrix" << rgbCameraMatrix;
+	if (!rgbDistCoeffs.empty()) fs << "rgbDistCoeffs" << rgbDistCoeffs;
+	if (!R.empty()) fs << "R" << R;
+	if (!T.empty()) fs << "T" << T;
+	if (!E.empty()) fs << "E" << E;
+	if (!F.empty()) fs << "F" << F;
 }
 
 void loadCalibration(const char *fname) {
@@ -245,6 +326,15 @@ void loadCalibration(const char *fname) {
 	fs["T"] >> T;
 	fs["E"] >> E;
 	fs["F"] >> F;
+
+	rvec.create(1,3,CV_64F);
+	Rodrigues(R,rvec);
+
+	cout << "irCameraMatrix" << endl; dump(irCameraMatrix);
+	cout << "rgbCameraMatrix" << endl; dump(rgbCameraMatrix);
+	cout << "T" << endl; dump(T);
+	cout << "rvec" << endl; dump(rvec);
+
 }
 
 int main() {
@@ -255,10 +345,14 @@ int main() {
 	// start in IR mode
 	CalibrationMode mode = CalibrationIr;
 	initCaptureMode(false, false, true);
+	// DEBUG
+//	CalibrationMode mode = CalibrationCheck;
+//	initCaptureMode(true, true, false);
+//	loadCalibration("calib1.home.xml"); // DEBUG
 
 	int alpha = 5000;
 
-	int key;
+	char key;
 	
 	stringstream sstream;
 
@@ -276,7 +370,8 @@ int main() {
 	
 	genPatternObjectPoints(patternSize, patternTileWidth, patternTileHeight, objectPoints);
 	
-	while ( (key = waitKey(1)) != 27 ) {
+	while ( (key = (char) waitKey(1)) != 27 ) {
+		//cout << "key : " << key << endl;
 		// handle keys
 		switch (key) {
 			case 't':
@@ -309,6 +404,12 @@ int main() {
 					initCaptureMode(true, false, false);
 				}
 				break;
+			case 'c':
+				if (mode != CalibrationCheck) {
+					mode = CalibrationCheck;
+					context.Shutdown();
+					initCaptureMode(true, true, false);
+				}
 			case 32:
 				capture = true;
 				break;
@@ -324,6 +425,10 @@ int main() {
 			case CalibrationRgb:
 				acquireRgb();
 				break;
+			case CalibrationCheck:
+				acquireDepth(255.0f / 4000.0f);
+				acquireRgb();
+				break;
 		}
 		
 		// calibration
@@ -331,7 +436,10 @@ int main() {
 			case CalibrationIr:			
 				if (tracking) {
 					cornersFound = findChessboardCorners(irMat8, patternSize, corners);
-					drawChessboardCorners(irMat8Rgb, patternSize, corners, cornersFound);					
+					if (cornersFound) {
+						cornerSubPix(irMat8, corners, subPixWindowSize, subPixZeroZone, subPixTerm);
+						drawChessboardCorners(irMat8Rgb, patternSize, Mat(corners), cornersFound);					
+					}
 					if (cornersFound && capture) {
 						irObjPoints.push_back(objectPoints);
 						irImgPoints.push_back(corners);
@@ -340,7 +448,9 @@ int main() {
 							frameSize,
 							irCameraMatrix, irDistCoeffs,
 							rvecs, tvecs
-						);
+						);						
+						cout << "irCameraMatrix" << endl; dump(irCameraMatrix);
+						cout << "irDistCoeffs" << endl; dump(irDistCoeffs);
 						if (!irCameraMatrix.empty()) {
 							undistort(irMat8Rgb, irMat8RgbUndist, irCameraMatrix, irDistCoeffs);
 							textLine(irMat8RgbUndist, "undistorted using calibration", 1, textColorBlue);
@@ -372,7 +482,10 @@ int main() {
 			case CalibrationRgb:
 				if (tracking) {
 					cornersFound = findChessboardCorners(rgbMat, patternSize, corners);
-					drawChessboardCorners(rgbMat, patternSize, corners, cornersFound);					
+					if (cornersFound) {
+						cornerSubPix(rgbMatGray, corners, subPixWindowSize, subPixZeroZone, subPixTerm);
+						drawChessboardCorners(rgbMat, patternSize, Mat(corners), cornersFound);
+					}
 					if (cornersFound && capture) {
 						rgbObjPoints.push_back(objectPoints);
 						rgbImgPoints.push_back(corners);
@@ -382,6 +495,8 @@ int main() {
 							rgbCameraMatrix, rgbDistCoeffs,
 							rvecs, tvecs
 						);
+						cout << "rgbCameraMatrix" << endl; dump(rgbCameraMatrix);
+						cout << "rgbDistCoeffs" << endl; dump(rgbDistCoeffs);						
 						if (!rgbCameraMatrix.empty()) {
 							undistort(rgbMat, rgbMatUndist, rgbCameraMatrix, rgbDistCoeffs);
 
@@ -414,7 +529,10 @@ int main() {
 			case CalibrationStereo:
 				if (tracking) {
 					cornersFound = findChessboardCorners(rgbMat, patternSize, corners);
-					drawChessboardCorners(rgbMat, patternSize, corners, cornersFound);					
+					if (cornersFound) {
+						cornerSubPix(rgbMatGray, corners, subPixWindowSize, subPixZeroZone, subPixTerm);
+						drawChessboardCorners(rgbMat, patternSize, Mat(corners), cornersFound);					
+					}
 					if (cornersFound && capture) {
 						// try to find points in rgb image as well
 						context.Shutdown();
@@ -424,7 +542,10 @@ int main() {
 						
 						vector<Point2f> irCorners;
 						cornersFound = findChessboardCorners(irMat8, patternSize, irCorners);
-						drawChessboardCorners(irMat8Rgb, patternSize, irCorners, cornersFound);
+						if (cornersFound) {
+							cornerSubPix(irMat8, corners, subPixWindowSize, subPixZeroZone, subPixTerm);
+							drawChessboardCorners(irMat8Rgb, patternSize, Mat(irCorners), cornersFound);
+						}
 						
 						if (cornersFound) { // we have found a set of correspondences!
 							stereoObjPoints.push_back(objectPoints);
@@ -439,6 +560,10 @@ int main() {
 								frameSize, R, T,
 								E, F
 							);
+							Rodrigues(R,rvec);
+
+							cout << "R" << endl; dump(R);
+							cout << "T" << endl; dump(T);
 							
 							if (stereoObjPoints.size() > 0) {
 								sstream.str(""); sstream << "number of views : " << stereoObjPoints.size();
@@ -472,7 +597,91 @@ int main() {
 				textLine(rgbMat, "mode : Stereo", 1, textColorBlue);
 				imshow(frameNameSecondary, rgbMat);
 			
-				break;			
+				break;
+			case CalibrationCheck:
+			{
+				// uncalibrated overlay
+				overlay(rgbMat, depthMat8, overlayMat);
+				textLine(overlayMat, "mode : Check", 1, textColorWhite);
+				textLine(overlayMat, "uncalibrated overlay", 2, textColorWhite);
+
+				// calibrated overlay
+				Mat depthMat16Calib(480, 640, CV_16UC1);
+				Mat depthMat8Calib(480, 640, CV_8UC1);
+				depthMat16Calib.setTo(0);
+
+				mapDepthToRgb(depthMat16, depthMat16Calib, rvec, T, irCameraMatrix, irDistCoeffs, rgbCameraMatrix, rgbDistCoeffs);
+				depthMat16Calib.convertTo(depthMat8Calib, CV_8UC1, 255.0f / 4000.0f);
+				overlay(rgbMat, depthMat8Calib, overlayCalibMat);
+				textLine(overlayCalibMat, "calibrated overlay", 2, textColorWhite);
+
+				imshow(frameNamePrimary, overlayMat);
+				imshow(frameNameSecondary, overlayCalibMat);
+
+				if (capture) { // capture json point clouds
+					int n = frameSize.width*frameSize.height;
+					int width = frameSize.width;
+
+					vector<Point3f> reconstructedPoints(n);
+					vector<Point2f> projectedPoints(n);
+
+					// reconstruct 3d points
+					reconstruct(depthMat16, irCameraMatrix, irDistCoeffs, reconstructedPoints);
+
+					// project 3d points to rgb coordinates
+					projectPoints(Mat(reconstructedPoints), -rvec, -T, rgbCameraMatrix, rgbDistCoeffs, projectedPoints);
+
+		ofstream json;
+  		json.open ("cloud.json");
+
+		
+
+					Mat thresh = depthMat16 < 1000 & depthMat16 > 0;
+					unsigned char *threshData = (unsigned char *) thresh.data;
+					unsigned char *rgbData = (unsigned char *) rgbMat.data;
+
+					json << "points = [";
+					for (int i=0; i<n; i++) {
+						if (threshData[i]) {
+							json << reconstructedPoints[i].x << ", " << reconstructedPoints[i].y << ", " << reconstructedPoints[i].z << ", " << endl;
+						}
+					}
+					json << "];" << endl << endl;
+
+					json << "colors = [";
+					for (int i=0; i<n; i++) {
+						if (threshData[i]) {
+							int u = (int) projectedPoints[i].x;
+							int v = (int) projectedPoints[i].y;
+							int j = u + v*width;
+							if (j >= 0 && j < n) {
+								//json << reconstructedPoints[i].x << ", " << reconstructedPoints[i].y << ", " << reconstructedPoints[i].z << ", " << endl;
+								json << (int)rgbData[3*j + 0] << ", " << (int)rgbData[3*j + 1] << ", " << (int)rgbData[3*j + 2] << ", " << endl;
+							} else {
+								json << "0, 0, 0," << endl;
+							}
+							
+							//json << reconstructedPoints[i].x << ", " << reconstructedPoints[i].y << ", " << reconstructedPoints[i].z << ", " << endl;
+						}
+					}
+					json << "];";
+
+		json.close();
+
+					//imshow("thresh" , thresh);
+				}
+
+				imshow("depthMat8", depthMat8);
+				imshow("rgbMat", rgbMat);
+	
+				// DEBUG
+//				Mat rgbTest(480, 640, CV_8UC1);
+//				rgbTest.setTo(Scalar(0,0,0));
+//				rgbMat.copyTo(rgbTest, depthMat16Calib < 1000 & depthMat16Calib > 0);
+//				imshow("rgbTest", rgbTest);
+
+			}
+			break;
 
 		}
 	
